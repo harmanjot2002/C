b1fea66379e94f3f4d4601049c3ccdece08864e5
harmanjot2002 <harmanjotkainth@gmail.com>
2022-12-30 16:41:26 +0530

    Formatted Input Functions

---
+++ formattedInputFunctions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+    int a,b,sum,x,y;
+    // clrscr();
+    // scanf("%d %d",&a,&b);
+    scanf("%4d %d",&a,&b);
+    // scanf("%f %d",&a,&b);
+    // y=scanf("%d",&a,&b);
+    // x=scanf("%d %d",&a,&b);
+    sum=a+b;
+    printf("Sum of %d and %d",a,b);
+    printf(" is: %d \n",sum);
+    // printf("%d \n",x);
+    // printf("%d \n",y);
+}
