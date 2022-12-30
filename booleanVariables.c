ea192cae40714804a010b0cfdaac2a965023ce96
harmanjot2002 <harmanjotkainth@gmail.com>
2022-12-30 20:50:12 +0530

    Boolean Variables

---
+++ booleanVariables.c
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+// #include<stdio.h>
+// #include<conio.h>
+void main(){
+    bool isProgrammingFun=true;
+    bool isFishTasty=false;
+    printf("%d \n",isProgrammingFun);
+    printf("%d",isFishTasty);
+}
+
