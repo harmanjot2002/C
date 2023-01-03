cf5a8f9c3241bbb21d046707c1ae722a539ba37e
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-03 13:56:48 +0530

    Class Practice

---
+++ class.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    printf("Hi! \nMyself Harmanjot Kaur from BE-CSE Chitkara University,Rajpura.\nI belong to Faridkot,Punjab.\nI was born on 12th December,2002.\nThanks!!\n");
+
+    int a;
+    int b;
+    printf("Enter 2 numbers to be added: \n");
+    scanf("%d %d",&a,&b);
+    printf("Sum is %d ",a+b);
+
+    int c=4,d=5;
+    printf("\n %d",c+d);
+}
