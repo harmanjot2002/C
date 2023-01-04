557fec0fbc2f6975e5656c2b151faf73477cc252
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-04 22:19:25 +0530

    Arrays

--- arrays.c
+++ arrays.c
@@ -13,4 +13,31 @@ int main(){
     for(int i=0;i<5;i++){
         printf("%d ",s[i]);
     }
+
+    //Sum nd average of 5 students
+    int marks[5];
+    float sum=0.0;
+    float avg;
+    printf("\nEnter marks of 5 subjects: ");
+    for(int i=0;i<5;i++)
+        scanf("%d",&marks[i]);
+    for(int i=0;i<5;i++){
+        sum+=marks[i];
+    }
+    avg=sum/5;
+    printf("%f %f \n",sum,avg);
+
+    //Count total no. of even and odd elements
+    int arr[10];
+    int even=0,odd=0;
+    for(int i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    for(int i=0;i<10;i++){
+        if(arr[i]%2==0)
+            even++;
+        else
+            odd++;
+    }
+    printf("Even are %d",even);
+    printf("\nodd are %d",odd);
 }
