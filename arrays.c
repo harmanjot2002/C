c779228d03eabeab52a2a3e1ddddbb374a313670
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-04 22:06:03 +0530

    Arrays

---
+++ arrays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define N 5;
+int main(){
+    int b;
+    int c[b=5/2];
+    //int s[];
+    int d[2*3];
+    // int s[-5];
+
+    int ba[]={9,0,8,7,6};
+    int s[5]={};
+    int a[5]={0};
+    for(int i=0;i<5;i++){
+        printf("%d ",s[i]);
+    }
+}
