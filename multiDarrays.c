a0a82d19fc57532163d3de5b0c75cb4b4542d2bb
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-07 14:27:04 +0530

    Multi-Dimensional Arrays

--- multiDarrays.c
+++ multiDarrays.c
@@ -93,5 +93,27 @@ int main(){
         printf("\n");
     }
     */
+
+   //Matrix Multiplication
+    int a[3][3],b[3][2],c[3][2],i,j,k,sum;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<2;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+     for(i=0;i<3;i++){
+        for(j=0;j<2;j++){
+            sum=0;
+            for(k=0;k<3;k++){
+                sum+=(a[i][k]*b[k][j]);
+                c[i][j]=sum;
+            }
+        }
+    }
     return 0;
 }
