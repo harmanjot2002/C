62b4fccc7992decd426cde8a2e161b739c430df3
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-07 14:20:25 +0530

    Multi-Dimensional Arrays

--- multiDarrays.c
+++ multiDarrays.c
@@ -61,6 +61,25 @@ int main(){
     */
 
     //Sum of 2 matrices
-    
+    int a[2][3],b[2][3],c[2][3],i,j;
+    printf("Enter values of first matrix: ");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Enter values of first matrix: ");
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            c[i][j]=a[i][j]+b[i][j];
+            printf("%d \t",c[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
