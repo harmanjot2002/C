331e558810a1999fb874151d00a9f5270663def8
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-09 17:41:31 +0530

    Strings

--- strings.c
+++ strings.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 int main(){
+    /*
     char name[30];
     printf("Enter name: ");
     scanf("%s",name);
     printf("%s",name);
+    printf("%s",name);
+    char name1[30];
+    printf("\nEnter name1: ");
+    gets(name1);
+    printf("%s",name1);
+    */
+   
+    char name1[30];
+    printf("\nEnter name1: ");
+    gets(name1);
+    printf("%s",name1);
     return 0;
-
 }
