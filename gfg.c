1bab6758ce75657f8ccd2728f98425bbc806f9c2
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-02 20:28:37 +0530

    GFG Practice

--- gfg.c
+++ gfg.c
@@ -299,11 +299,19 @@ int main()
 
     //Count distinct elements in array
     int n;
+    int count=0;
     scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]!=arr[j]){
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
     return 0;
 }
