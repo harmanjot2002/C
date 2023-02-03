2f92762e943313093f9d6d3057a325ff770750a1
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-03 13:24:58 +0530

    Pointers

--- pointers.c
+++ pointers.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 int main(){
+    /*
     int a[]={3,2,67,0,56};
     int *p;
+    */
     /*
     p=a;
     printf("%d %d",*p++,*p++);
     */
-   p=&a[2];
-   printf("%d %d %d",--(*p),--(*p),--(*p));
+    /*
+    p=&a[2];
+    printf("%d %d %d",--(*p),--(*p),--(*p));
+    */
+   int a[]={10,11,-1,56,67,5,4};
+   int *p,*q;
+   p=a;
+   q=&a[0]+3;
+   printf("%d %d %d\n",(*p)++,(*p)++,*(++p));
+   printf("%d\n",*p);
+   printf("%d\n",(*p)++);
+   printf("%d\n",(*p)++);
+   q--;
+   printf("%d\n",(*(q+2))--);
+   printf("%d\n",*(p+2)-2);
+   printf("%d\n",*(p++ -2)-1);
 }
