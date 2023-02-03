80e87f17c7623c999a7a1f08c1b98544f439b6ca
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-03 19:41:40 +0530

    Pointers

--- pointers.c
+++ pointers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     /*
     int a[]={3,2,67,0,56};
     int *p;
@@ -12,16 +13,25 @@ int main(){
     p=&a[2];
     printf("%d %d %d",--(*p),--(*p),--(*p));
     */
-   int a[]={10,11,-1,56,67,5,4};
-   int *p,*q;
-   p=a;
-   q=&a[0]+3;
-   printf("%d %d %d\n",(*p)++,(*p)++,*(++p));
-   printf("%d\n",*p);
-   printf("%d\n",(*p)++);
-   printf("%d\n",(*p)++);
-   q--;
-   printf("%d\n",(*(q+2))--);
-   printf("%d\n",*(p+2)-2);
-   printf("%d\n",*(p++ -2)-1);
+
+    /*
+    int a[]={10,11,-1,56,67,5,4};
+    int *p,*q;
+    p=a;
+    q=&a[0]+3;
+    printf("%d %d %d\n",(*p)++,(*p)++,*(++p));
+    printf("%d\n",*p);
+    printf("%d\n",(*p)++);
+    printf("%d\n",(*p)++);
+    q--;
+    printf("%d\n",(*(q+2))--);
+    printf("%d\n",*(p+2)-2);
+    printf("%d\n",*(p++ -2)-1);
+    */
+
+   const int a=-11;
+   int *p=&a;
+   *p=10;
+   printf("%d",a);
+    return 0;
 }
