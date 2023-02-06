dd69ee74122620aecc71592a9dc8c08a47cf8550
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-06 13:51:37 +0530

    Patterns

--- patterns.c
+++ patterns.c
@@ -382,7 +382,30 @@ for(int i=1;i<=n;i++){
 }
 */
 
-//X-Pattern
-int n;
-scanf("%d",&n);
+//Hackerrank pattern
+    int n;
+    scanf("%d",&n);
+    int size=2*n-1;
+    int start=0;
+    int end=size-1;
+    int a[size][size];
+    while(n!=0){
+        for(int i=start;i<=end;i++){
+            for(int j=start;j<=end;j++){
+                if(i==start || j==start || i==end ||j==end){
+                    a[i][j]=n;
+                }
+            }
+        }
+        ++start;
+        --end;
+        --n;
+    }
+    for (int i =0; i<size; i++) {
+        for(int j = 0;j<size;j++){
+            printf("%d ",a[i][j]);            
+        }
+        printf("\n");
+    }
+
 }
