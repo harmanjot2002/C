05f14e038dd7b27562706ceeb7080404e5a4dc5c
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-10 09:24:14 +0530

    Arrays

--- arrays.c
+++ arrays.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #define N 5;
 int main(){
+    /*
     int b;
     int c[b=5/2];
     //int s[];
     int d[2*3];
     // int s[-5];
+    */
 
+   /*
     int ba[]={9,0,8,7,6};
     int s[5]={};
     int a[5]={0};
     for(int i=0;i<5;i++){
         printf("%d ",s[i]);
     }
+    */
 
+   /*
     //Sum nd average of 5 students
     int marks[5];
     float sum=0.0;
@@ -26,7 +31,9 @@ int main(){
     }
     avg=sum/5;
     printf("%f %f \n",sum,avg);
+    */
 
+   /*
     //Count total no. of even and odd elements
     int arr[10];
     int even=0,odd=0;
@@ -40,7 +47,9 @@ int main(){
     }
     printf("Even are %d",even);
     printf("\nodd are %d",odd);
+    */
 
+   /*
     //Sum of 2 arrays
     int a1[5];
     int a2[5];
@@ -55,4 +64,28 @@ int main(){
         sumArr[i]=a1[i]+a2[i];
         printf("Sum at index %d is %d\n",i,sumArr[i]);
     }
+    */
+
+   
+    //Sort an array
+    int n2;
+    int arr2[n2];
+    printf("Enter elements in array: ");
+    scanf("%d",&n2);
+    for(int i=0;i<n2;i++){
+        scanf("%d",&arr2[i]);
+    }
+    for(int i=0;i<n2;i++){
+        for(int j=i+1;j<n2;j++){
+            if(arr2[i]>arr2[j]){
+                int t=arr2[i];
+                arr2[i]=arr2[j];
+                arr2[j]=t;
+            }
+        }
+    }
+    for(int i=0;i<n2;i++){
+        printf("%d",arr2[i]);
+    }
+    return 0;
 }
