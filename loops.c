b5c51356f8ef77253cab3d986921039ce31aa8d6
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-17 18:18:37 +0530

    Loops

---
+++ loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    //Compile-time error
+    /*
+    int i=1;
+    while(){
+        printf("%d",i);
+        i++;
+    }
+    printf("End of program");
+    */
+    return 0;
+}
