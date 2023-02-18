5cf63a750a652ecc785779ec10ddbad65709889c
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-18 14:06:46 +0530

    Practice Codes

--- practice.c
+++ practice.c
@@ -13,6 +13,24 @@ int main(){
         printf("Not leap year");
     */
 
-   
+   //Transpose of array
+   /*
+   int a[2][3];
+   printf("Enter elements of array: ");
+   for(int i=0;i<2;i++){
+    for(int j=0;j<3;j++){
+        scanf("%d",&a[i][j]);
+    }
+   }
+   printf("Transpose of given array is:\n");
+   for(int i=0;i<3;i++){
+    for(int j=0;j<2;j++){
+        printf("%d ",a[j][i]);
+    }
+    printf("\n");
+   }
+   */
+
+  
     return 0;
 }
