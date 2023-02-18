4fcc6cb7e7e9d0e83fb90407db7b00c4d050a514
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-18 11:09:15 +0530

    Dynamic Memory Allocation

--- dynamicMemoryAllocation.c
+++ dynamicMemoryAllocation.c
@@ -6,12 +6,20 @@ int main(){
     int *ptr;
     printf("Enter no. of values to be entered ");
     scanf("%d",&n);
-    ptr=(int *) malloc(n*sizeof(int));
+    //ptr=(int *) malloc(n*sizeof(int));
+    ptr=(int *) calloc(n,sizeof(int));
     printf("Enter values: ");
     for(int i=0;i<n;i++)
         scanf("%d",ptr+i);
     printf("Entered values are: ");
     for(int i=0;i<n;i++)
         printf("%d ",*ptr+i);
-    free(ptr);
+    printf("\nEnter updated value of n: ");
+    scanf("%d",&n);
+    int *ptr1=(int *) realloc(ptr,n*sizeof(int));
+    printf("Previous address=%d,new address=%d",ptr,ptr1);
+    printf("\nValues are: ");
+    for(int i=0;i<n;i++)
+        printf("%d ",*(ptr+i));
+    free(ptr1);
 }
