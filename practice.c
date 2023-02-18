a88f891fee02ec341db6a5f1b83308935f27c489
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-18 20:58:53 +0530

    Practice Codes

--- practice.c
+++ practice.c
@@ -72,6 +72,24 @@ int main(){
     printf("LCM of %d and %d is %d",n,m,ans);
     */
 
+   //Check prime no. or not
+   /*
+   int n;
+   printf("Enter a number:");
+   scanf("%d",&n);
+   int flag=0;
+   for(int i=2;i<n;i++){
+    if(n%i==0){
+        flag=1;
+        break;
+    }
+   }
+    if(flag==0)
+        printf("Prime");
+    else    
+        printf("Not Prime");
+    */
+
    
     return 0;
 }
