eeffc3cdb3a6ff12aff5822c170501f97ee47ed9
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-18 09:19:19 +0530

    Pointers

--- pointers.c
+++ pointers.c
@@ -54,5 +54,14 @@ int main()
    printf("%d\n",p);
 */
     
+    int a[]={10,11,-1,56,67,5,4};
+    int *p,*q;
+    p=a;
+    printf("%d\n",*p);
+    printf("%d %d %d\n",(*p)++,*p++,*++p);
+    q=p+3;
+    printf("%d\n",*q-3);
+    printf("%d\n",*--p+5);
+    printf("%d\n",*p+*q);
    return 0;
 }
