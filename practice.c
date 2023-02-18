091433796235e5f287de5124929a210d585db09a
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-18 14:01:55 +0530

    Practice Codes

---
+++ practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    //Check leap year
+    /*
+    int year;
+    scanf("%d",&year);
+    if(year%4==0 && year%100!=0)
+        printf("Leap year");
+    else if(year%400==0)
+        printf("Leap year");
+    else
+        printf("Not leap year");
+    */
+
+   
+    return 0;
+}
