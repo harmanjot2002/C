3eb050de933ae505105ad80d2ccbbb52db7c4392
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-19 13:17:41 +0530

    Practice Codes

--- practice.c
+++ practice.c
@@ -688,6 +688,26 @@ int main(){
     */
 
    //Bitwise Operators
-    
+   /*
+    5 4
+    2 3 3
+   */
+    int n,k;
+    scanf("%d %d",&n,&k);
+    int maxAnd = 0,maxOr = 0,maxXor = 0;
+    for (int i=1; i<=n; i++) {
+        for (int j=i+1; j<=n; j++) {
+            if (((i&j) > maxAnd) && ((i&j) < k)) {
+                maxAnd = i&j;
+            }
+            if (((i|j) > maxOr) && ((i|j) < k)) {
+                maxOr = i|j;
+            }
+            if (((i^j) > maxXor) && ((i^j) < k)) {
+                maxXor = i^j;
+            }
+        }
+    }
+    printf("%d %d %d", maxAnd, maxOr, maxXor);
     return 0;
 }
