1a7f9b182367214592d372f9789f49577183d94f
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-24 15:00:11 +0530

    Typedef

--- typedef.c
+++ typedef.c
@@ -6,10 +6,15 @@ typedef struct student{
     char name[20];
 }stu;
 
+typedef int integer;
+
 int main(){
     stu s1={2,34.5,"Harman"};
     printf("%d\n",s1.rollno);
     printf("%f\n",s1.marks);
     printf("%s\n",s1.name);
+
+    integer b=12;
+    printf("%d",b);
     return 0;
 }
