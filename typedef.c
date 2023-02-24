c1848b194c4f1050be3e7a6fe4155a5316e39aa1
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-24 14:58:47 +0530

    Typedef

---
+++ typedef.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct student{
+    int rollno;
+    float marks;
+    char name[20];
+}typedef;
+
+int main(){
+    struct student s1={2,34.5,"Harman"};
+    printf("%d\n",s1.rollno);
+    printf("%f\n",s1.marks);
+    printf("%s\n",s1.name);
+    return 0;
+}
