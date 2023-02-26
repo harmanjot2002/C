e87f4f4951d59f3ffc8c198d45448b3fb82d4dfe
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-26 19:39:33 +0530

    Copy contents from one file to other

--- copyContentOfFileToOther.c
+++ copyContentOfFileToOther.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include<stdlib.h>
+
+void main(){
+    FILE *fp1,*fp2;
+    char c;
+    fp1=fopen("src.txt","r");
+    if(fp1==NULL){
+        printf("ERROR");
+        exit(1);
+    }
+    fp2=fopen("dst.txt","w");
+    while((c=fgetc(fp1))!=EOF){
+        fputc(c,fp2);
+    }
+    printf("Successfully copied");
+    fclose(fp1);
+    fclose(fp2);
+}
